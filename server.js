d0491ea3201a7967be3f786811d56f6f6eb9ea20
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-06-05 15:49:28 -0700

    update

--- server.js
+++ server.js
@@ -30,26 +30,14 @@ app.listen(PORT, function () {
 
 // #########################################################################
 
-// var mysql = require("mysql");
+var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//     host: "localhost",
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-//     // Your port; if not 3306
-//     port: 3306,
-
-//     // Your username
-//     user: "root",
-
-//     // Your password
-//     password: "root",
-//     database: "jaws"
-// });
-
-// connection.connect(function (err) {
-//     if (err) throw err;
-//     console.log("connected as id " + connection.threadId + "\n");
-// });
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+});
 
 // function addName(fn, ln) {
 //     connection.query(
